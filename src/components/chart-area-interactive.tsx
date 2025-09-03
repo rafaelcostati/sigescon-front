a93333c0ffb9198ceb119702a6b3e348e581f6e5
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-09-03 14:14:04 -0300

    alteração dashboard por período

--- src/components/chart-area-interactive.tsx
+++ src/components/chart-area-interactive.tsx
@@ -166,12 +166,12 @@ export function ChartAreaInteractive() {
   return (
     <Card className="@container/card">
       <CardHeader>
-        <CardTitle>Total Visitors</CardTitle>
+        <CardTitle>Contratos próximos de vencer</CardTitle>
         <CardDescription>
           <span className="hidden @[540px]/card:block">
-            Total for the last 3 months
+            Total para os próximos 3 meses
           </span>
-          <span className="@[540px]/card:hidden">Last 3 months</span>
+          <span className="@[540px]/card:hidden">Próximos 3 meses</span>
         </CardDescription>
         <CardAction>
           <ToggleGroup
@@ -181,9 +181,9 @@ export function ChartAreaInteractive() {
             variant="outline"
             className="hidden *:data-[slot=toggle-group-item]:!px-4 @[767px]/card:flex"
           >
-            <ToggleGroupItem value="90d">Last 3 months</ToggleGroupItem>
-            <ToggleGroupItem value="30d">Last 30 days</ToggleGroupItem>
-            <ToggleGroupItem value="7d">Last 7 days</ToggleGroupItem>
+            <ToggleGroupItem value="90d">Próximos 3 meses</ToggleGroupItem>
+            <ToggleGroupItem value="30d">Próximos 30 dias</ToggleGroupItem>
+            <ToggleGroupItem value="7d">Próximos 7 dias</ToggleGroupItem>
           </ToggleGroup>
           <Select value={timeRange} onValueChange={setTimeRange}>
             <SelectTrigger
