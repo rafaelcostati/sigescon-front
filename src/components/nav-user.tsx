682a39dbb9b6960a0f13c3415b8bb3bbba19ed07
rafaelcostati <rafaelblur@gmail.com>
2025-09-23 14:00:46 -0300

    dfdf

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -37,7 +37,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
+  
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
